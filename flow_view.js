890fffaaf6687052286a1a3c4523c1f81cc5bff4
HAGIWARA Manabu <manabu.hagiwara@okinawaopenlabs.org>
2017-05-07 20:55:57 +0900

    Fix arc, text label position and rotation.

--- flow_view.js
+++ flow_view.js
@@ -39,7 +39,7 @@ function gen_edge_table(flow_paths) {
 function gen_path_edges(edge_table) {
     // serialize order by switch/port
     var all_edges = [];
-    Object.keys(edge_table).forEach(function(sw) {
+    Object.keys(edge_table).sort().forEach(function(sw) {
         var port_table = edge_table[sw];
         Object.keys(port_table).sort().forEach(function(port) {
             all_edges = all_edges.concat(port_table[port])
@@ -51,16 +51,11 @@ function gen_path_edges(edge_table) {
         edge["index"] = index;
         index = index + 1;
     });
-    console.log(all_edges);
 
     return all_edges;
 }
 
 function draw_flow_data(flow_paths) {
-    // console.log(flow_data);
-    // console.log(nested_flow_data);
-    console.log(flow_paths);
-
     var size = 600;
     var svg = d3.select("body")
         .select("div#flow_view")
@@ -71,46 +66,57 @@ function draw_flow_data(flow_paths) {
         })
         .append("g")
         .attr("transform",
-            "translate(" + size / 2 + "," + size / 2 + ")"); // centering
+            "translate(" + size/2 + "," + size/2 + ")"); // centering
 
     // list of all edges (flow rule edge)
     var edge_table = gen_edge_table(flow_paths);
     var all_edges = gen_path_edges(edge_table);
+    console.log(edge_table);
+    console.log(all_edges);
 
     var lradius = size / 3;
     var cradius = lradius * Math.PI / all_edges.length * 0.8;
-    var theta = d3.scalePoint()
+    var rad = d3.scalePoint()
         .domain(Object.keys(all_edges).concat(all_edges.length))
         .range([0, 2 * Math.PI]);
+    var deg = d3.scalePoint()
+        .domain(Object.keys(all_edges).concat(all_edges.length))
+        .range([0, 360]);
 
     function descr(d) {
-        return d.sw + " " + d.port
+        return "[" + d.index + "] " + d.sw + " " + d.port
         + " (" + d.flow.in_port + "->" + d.flow.output + ")";
     }
 
     // draw arc
+    // ref: Banded Arcs - bl.ocks.org
+    // https://bl.ocks.org/mbostock/938288
     var port_arc = d3.arc()
         .innerRadius(lradius)
-        .outerRadius(lradius + cradius*2);
+        .outerRadius(lradius + cradius*3);
     var sw_arc = d3.arc()
-        .innerRadius(lradius + cradius*2.2)
-        .outerRadius(lradius + cradius*4.2);
+        .innerRadius(lradius + cradius*4)
+        .outerRadius(lradius + cradius*7);
+    var aaoff = Math.PI/2; // arc angle offset
     Object.keys(edge_table).forEach(function(sw) {
         var port_table = edge_table[sw];
         var port_list = Object.keys(port_table);
-        port_list.forEach(function(port) {
+        port_list.sort().forEach(function(port) {
             var flows = port_table[port];
+            var start = flows[0].index;
+            var end = flows[flows.length-1].index;
+            // port arc
             svg.append("path")
                 .attrs({
                     "class" : "arc port",
                     "d" : port_arc
-                        .startAngle(theta(flows[0].index))
-                        .endAngle(theta(flows[flows.length-1].index))
+                        .startAngle(rad(start) + aaoff)
+                        .endAngle(rad(end) + aaoff)
                 })
                 .append("title")
                 .text(sw + ", port:" + port);
         });
-        // TODO switch arc now work well...
+        // switch arc
         var head = port_table[port_list[0]];
         var head_index = head[0].index;
         var tail = port_table[port_list[port_list.length-1]];
@@ -119,34 +125,23 @@ function draw_flow_data(flow_paths) {
             .attrs({
                 "class" : "arc switch",
                 "d" : sw_arc
-                    .startAngle(theta(head_index))
-                    .endAngle(theta(tail_index))
+                    .startAngle(rad(head_index) + aaoff)
+                    .endAngle(rad(tail_index) + aaoff)
             })
             .append("title")
             .text(sw);
     });
-
-    // draw text label
-    svg.selectAll("text")
-        .data(all_edges)
-        .enter()
-        .append("text")
-        .attrs({
-            "x": function(d) { return pos_x(lradius, theta(d.index)); },
-            "y": function(d) { return pos_y(lradius, theta(d.index)); }
-        })
-        .text(descr);
 
     // draw path
     var mid = 0.3;
     var line = d3.line()
         .curve(d3.curveBundle.beta(0.9))
         .x(function(d, i) {
-            var x = pos_x(lradius, theta(d));
+            var x = pos_x(lradius, rad(d));
             return (0 < i && i < 3) ? mid * x : x;
         })
         .y(function(d, i) {
-            var y = pos_y(lradius, theta(d));
+            var y = pos_y(lradius, rad(d));
             return (0 < i && i < 3) ? mid * y : y;
         });
     flow_paths.forEach(function(flow_path) {
@@ -176,13 +171,42 @@ function draw_flow_data(flow_paths) {
         .append("circle")
         .attrs({
             "class" : function(d) { return d.type + " " + d.flow.tags; },
-            "cx": function(d) { return pos_x(lradius, theta(d.index)); },
-            "cy": function(d) { return pos_y(lradius, theta(d.index)); },
+            "cx": function(d) { return pos_x(lradius, rad(d.index)); },
+            "cy": function(d) { return pos_y(lradius, rad(d.index)); },
             "r" : cradius
         })
         .on("mouseover", function() { mouse_event(this, true); })
         .on("mouseout", function() { mouse_event(this, false); })
         .append("title")
+        .text(descr);
+
+    // draw text label
+    // ref: Interactive text rotation with d3.js - bl.ocks.org
+    // http://bl.ocks.org/d3noob/10633421
+    svg.selectAll("text")
+        .data(all_edges)
+        .enter()
+        .append("text")
+        .attrs({
+            "dx": function(d) {
+                var t = rad(d.index);
+                var dx = cradius * 1.2;
+                return (Math.PI/2 < t && t < 3*Math.PI/2) ? -dx : dx;
+            },
+            "dy": cradius/2,
+            "text-anchor": function(d) {
+                var t = rad(d.index);
+                return (Math.PI/2 < t && t < 3*Math.PI/2) ? "end" : "start";
+            },
+            "transform" : function(d) {
+                var t = deg(d.index);
+                var angle = (90 < t && t < 270) ? t + 180 : t;
+                return "translate("
+                    + pos_x(lradius, rad(d.index)) + ","
+                    + pos_y(lradius, rad(d.index)) + ") "
+                    + "rotate(" + angle + ")"; // use degree
+            }
+        })
         .text(descr);
 }
 
@@ -191,6 +215,3 @@ function gen_path_index(flow_path) {
     var dst = flow_path.dst.index;
     return [src, src, dst, dst];
 }
-
-// ref
-// Banded Arcs - bl.ocks.org https://bl.ocks.org/mbostock/938288
