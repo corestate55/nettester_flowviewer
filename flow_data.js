863aa8190b4e1b990a7b529d31535e5bc59df0e1
HAGIWARA Manabu <manabu.hagiwara@okinawaopenlabs.org>
2017-06-10 17:17:44 +0900

    Use d3.cluster to draw nodes/paths.

--- flow_data.js
+++ flow_data.js
@@ -7,14 +7,14 @@ function add_index_to_flows(flows) {
     return flows;
 }
 
-function build_flow_data(ssw_textarea, psw_textarea) {
-    var ssw_flows = build_flow_data_from(ssw_textarea);
-    var psw_flows = build_flow_data_from(psw_textarea);
+function parse_flows(ssw_textarea, psw_textarea) {
+    var ssw_flows = parse_flows_from(ssw_textarea);
+    var psw_flows = parse_flows_from(psw_textarea);
 
     return add_index_to_flows(ssw_flows.concat(psw_flows));
 }
 
-function build_flow_data_from(flow_table_textarea) {
+function parse_flows_from(flow_table_textarea) {
     var lines = flow_table_textarea.value
         .split("\n")
         .filter(function (thisobj) {
@@ -38,6 +38,7 @@ function build_flow_data_from(flow_table_textarea) {
             "flood_mark" : false
         };
 
+        // parse numeric values
         numeric_values.forEach(function (nvalue) {
             var re = new RegExp(nvalue + "=(\\d+)");
             var result = line.match(re);
@@ -46,6 +47,7 @@ function build_flow_data_from(flow_table_textarea) {
                 data[nvalue] = Number(result[1]);
             }
         });
+        // parse mac address
         mac_values.forEach(function (svalue) {
             var re = new RegExp(svalue + "=(\\S+)");
             var result = line.match(re);
@@ -54,7 +56,7 @@ function build_flow_data_from(flow_table_textarea) {
                 data["mac"] = result[1];
             }
         });
-
+        // parse actions
         var result = line.match(/actions=(\S+)/);
         if(result) {
             var actions_str = result[1];
@@ -71,9 +73,154 @@ function build_flow_data_from(flow_table_textarea) {
             }
             res = actions_str.match(/mod_vlan_vid:(\d+)/);
             if(res) {
+                // for exclusive mode wire
                 data.vlan = res[1];
             }
         }
         return data;
+    });
+}
+
+function generate_node_name(switch_name, port, flow_index, dir) {
+    return [switch_name, "port", port, "flow", flow_index, dir].join("_");
+}
+
+function convert_mac_to_tag(mac) {
+    return "mac_" + mac.replace(/:/g, "");
+}
+
+function generate_tags(data) {
+    var tags = [ "sw_" + data.switch ];
+    if(!data.action_flood && data.mac) {
+        tags.push(convert_mac_to_tag(data.mac));
+    }
+    if(data.flood_mark) {
+        tags.push("flood");
+    }
+    if (data.switch === "ssw") {
+        if(data.in_port > 1) { // ssw edge port
+            tags.push("to_testee");
+        } else {
+            tags.push("to_tester");
+        }
+    } else {
+        if(data.in_port > 1
+            && (data.output === 1 || data.action_flood)) {
+            // psw edge port
+            tags.push("to_tester");
+        } else if(data.in_port === 1 && data.output > 1){
+            tags.push("to_testee");
+        } else {
+            tags.push("rule_" + data.index);
+        }
+    }
+    return tags.join(" ");
+}
+
+function generate_node(data, dir) {
+    var sport, dport;
+    if(dir === "source") {
+        sport = data.in_port;
+        dport = data.output;
+    } else {
+        dport = data.in_port;
+        sport = data.output;
+    }
+    return {
+        "name" : generate_node_name(data.switch, sport, data.index, dir),
+        "switch" : data.switch,
+        "tags" : generate_tags(data),
+        "flow_index" : data.index,
+        "type" : dir,
+        "source_port" : sport,
+        "target_port" : dport,
+        "data" : data
+    };
+}
+
+function build_flows(flows) {
+    var nodes = [];
+    flows.forEach(function(d) {
+        nodes.push(generate_node(d, "source"));
+        nodes.push(generate_node(d, "target"));
+    });
+    return nodes;
+}
+
+function nest_nodes(nodes) {
+    return d3.nest()
+        .key(function(d) { return d.switch; })
+        .sortKeys(d3.ascending)
+        .key(function(d) { return d.source_port; })
+        .sortKeys(function(a,b) { return a - b; })
+        .entries(nodes);
+}
+
+function complement_tags(paths) {
+    // create mac addr table
+    var mac_table = {
+        "ssw" : {},
+        "psw" : {}
+    };
+    function record_table(data) {
+        if(data.source_port > 1 && !data.data.action_flood && data.data.mac) {
+            // port1 of ssw/psw is inter_switch link
+            // so, port1 is not tied to any host(mac addr).
+            if(!mac_table[data.switch][data.source_port]) {
+                mac_table[data.switch][data.source_port] = [];
+            }
+            mac_table[data.switch][data.source_port].push(data.data.mac);
+        }
+    }
+    paths.forEach(function(path) {
+        record_table(path.source.data);
+        record_table(path.target.data);
+    });
+
+    // complement tag info
+    function complement_tag_info(source, target) {
+        var port_macs = mac_table[source.switch][source.source_port] || null;
+        if(!source.data.mac && port_macs) {
+            port_macs.forEach(function(mac) {
+                var mac_tag = convert_mac_to_tag(mac);
+                source.tags = source.tags + " " + mac_tag;
+                target.tags = target.tags + " " + mac_tag;
+            });
+        }
+    }
+    paths.forEach(function(path) {
+        var source = path.source.data;
+        var target = path.target.data;
+        complement_tag_info(source, target);
+    });
+    return paths;
+}
+
+function generate_path(nodes) {
+    var paths = [];
+    var nodes_name_map = d3.map(nodes, function(d) {
+        return d.data.name;
+    });
+    nodes.forEach(function(d) {
+        if(d.data.type === "source") {
+            var target_name = generate_node_name(
+                d.data.switch, d.data.target_port, d.data.flow_index, "target");
+            paths.push({
+                "source" : d,
+                "target" : nodes_name_map.get(target_name)
+            });
+        }
+    });
+
+    return complement_tags(paths);
+}
+
+function stratify_nested_nodes(nodes) {
+    var data = {
+        "key" : "whole switches",
+        "values": nest_nodes(nodes)
+    };
+    return d3.hierarchy(data, function(d) {
+        return d.values;
     });
 }
--- flow_view.js
+++ flow_view.js
@@ -1,240 +1,151 @@
 "use strict";
 
-function pos_x(radius, theta) {
-    return radius * Math.cos(theta);
-}
-
-function pos_y(radius, theta) {
-    return radius * Math.sin(theta);
-}
-
-function gen_edge_table(flow_paths) {
-    var edge_table = {
-        "$psw": {},
-        "$ssw": {}
-    };
-
-    flow_paths.forEach(function (path) {
-        path.src.type = "src";
-        path.src.flow = path.flow;
-        path.src.pair = path.dst;
-        path.dst.type = "dst";
-        path.dst.flow = path.flow;
-        path.dst.pair = path.src;
-
-        if (edge_table[path.src.sw][path.src.port]) {
-            edge_table[path.src.sw][path.src.port].push(path.src);
-        } else {
-            edge_table[path.src.sw][path.src.port] = [path.src];
-        }
-        if (edge_table[path.dst.sw][path.dst.port]) {
-            edge_table[path.dst.sw][path.dst.port].push(path.dst);
-        } else {
-            edge_table[path.dst.sw][path.dst.port] = [path.dst];
-        }
-    });
-    return edge_table;
-}
-
-function gen_path_edges(edge_table) {
-    // serialize order by switch/port
-    var all_edges = [];
-    Object.keys(edge_table).sort().forEach(function(sw) {
-        var port_table = edge_table[sw];
-        Object.keys(port_table).sort().forEach(function(port) {
-            all_edges = all_edges.concat(port_table[port])
-        });
-    });
-    // add index number
-    var index = 0;
-    all_edges.forEach(function(edge) {
-        edge["index"] = index;
-        index = index + 1;
-    });
-
-    return all_edges;
-}
-
-function draw_flow_data(flow_paths) {
+function draw_flow_data(nodes, paths) {
     var width = 700;
-    var height = 500;
+    var height = 700;
+    var radius = 0.7 * d3.min([width, height]) / 2;
     var svg = d3.select("body")
         .select("div#flow_view")
         .append("svg")
         .attrs({
             "id" : "flow_view_canvas",
-            "width": width,
-            "height": height
+            "width" : width,
+            "height" : height
         })
         .append("g")
         .attr("transform",
             "translate(" + width/2 + "," + height/2 + ")"); // centering
 
-    // list of all edges (flow rule edge)
-    var edge_table = gen_edge_table(flow_paths);
-    var all_edges = gen_path_edges(edge_table);
+    // layout
+    var layout = d3.cluster().size([360, radius]);
+    layout(nodes);
+    var node_size = 0.8 * radius * Math.PI / nodes.descendants().length;
 
-    // parameters
-    var lradius = Math.min(height, width) / 3;
-    var cradius = lradius * Math.PI / all_edges.length * 0.9;
-    var rad = d3.scalePoint()
-        .domain(Object.keys(all_edges).concat(all_edges.length))
-        .range([0, 2 * Math.PI]);
-    var deg = d3.scalePoint()
-        .domain(Object.keys(all_edges).concat(all_edges.length))
-        .range([0, 360]);
-
-    // draw arc
-    // ref: Banded Arcs - bl.ocks.org
-    // https://bl.ocks.org/mbostock/938288
-
-    function arc_mouse_event(th, is_mouse_over) {
-        d3.select(th).classed("targeted", is_mouse_over);
-    }
-
-    var port_arc = d3.arc()
-        .innerRadius(lradius - cradius*3)
-        .outerRadius(lradius);
-    var sw_arc = d3.arc()
-        .innerRadius(lradius - cradius*7)
-        .outerRadius(lradius - cradius*4);
-    var aaoff = Math.PI/2; // arc angle offset
-
-    Object.keys(edge_table).forEach(function(sw) {
-        var port_table = edge_table[sw];
-        var port_list = Object.keys(port_table);
-        port_list.sort().forEach(function(port) {
-            var flows = port_table[port];
-            var start = flows[0].index;
-            var end = flows[flows.length-1].index;
-            // port arc
-            svg.append("path")
-                .attrs({
-                    "class" : "arc port",
-                    "d" : port_arc
-                        .startAngle(rad(start) + aaoff)
-                        .endAngle(rad(end) + aaoff)
-                })
-                .on("mouseover", function() { arc_mouse_event(this, true); })
-                .on("mouseout", function() { arc_mouse_event(this, false); })
-                .append("title")
-                .text((sw + ", port:" + port).replace(/\$/g, ""));
+    // draw lines
+    var line = d3.radialLine()
+        .curve(d3.curveBundle.beta(0.85))
+        .radius(function(d) { return d.y; })
+        .angle(function(d) { return path_angle(d.x); });
+    svg.selectAll("path")
+        .data(paths)
+        .enter()
+        .append("path")
+        .attrs({
+            "class" : function(d) {
+                return d.source.data.tags;
+            },
+            "d" : function(d) {
+                return line(d.source.path(d.target));
+            }
         });
-        // switch arc
-        var head = port_table[port_list[0]];
-        var head_index = head[0].index;
-        var tail = port_table[port_list[port_list.length-1]];
-        var tail_index = tail[tail.length-1].index;
-        svg.append("path")
-            .attrs({
-                "class" : "arc switch",
-                "d" : sw_arc
-                    .startAngle(rad(head_index) + aaoff)
-                    .endAngle(rad(tail_index) + aaoff)
-            })
-            .on("mouseover", function() { arc_mouse_event(this, true); })
-            .on("mouseout", function() { arc_mouse_event(this, false); })
-            .append("title")
-            .text(sw.replace(/\$/g, ""));
-    });
 
-    // draw path
-
-    var mid = 0.3;
-    var line = d3.line()
-        .curve(d3.curveBundle.beta(0.9))
-        .x(function(d, i) {
-            var x = pos_x(lradius, rad(d));
-            return (0 < i && i < 3) ? mid * x : x;
-        })
-        .y(function(d, i) {
-            var y = pos_y(lradius, rad(d));
-            return (0 < i && i < 3) ? mid * y : y;
-        });
-    flow_paths.forEach(function(flow_path) {
-        svg.append("path")
-            .attrs({
-                "class" : flow_path.flow.tags,
-                "d" : line(gen_path_index(flow_path))
-            });
-    });
-
-    // draw edge circle
-
-    function descr(d) {
-        var str = "[" + d.index + "] " + d.sw + " " + d.port
-            + " (" + d.flow.in_port + "->" + d.flow.output + ")";
-        return str.replace(/\$/g, "");
-    }
-
-    function edge_mouse_event(th, is_mouse_over) {
-        var class_list = th.getAttribute("class").split(" ");
-        var class_words = [""];
-
-        function push_to(re_list) {
-            re_list.forEach(function(re) {
-                var word = class_list.find(function(class_str) {
-                    return class_str.match(re);
-                });
-                if(word) class_words.push(word);
-            });
-        }
-        push_to([/mac_/, /to_/, /flood/, /rule_/]);
-        var class_str = class_words.join(".");
-        if(class_str.length > 1) {
-            svg.selectAll(class_str)
-                .classed("targeted", is_mouse_over);
-        }
-    }
-
+    // draw nodes
     svg.selectAll("circle")
-        .data(all_edges)
+        .data(nodes.descendants())
         .enter()
         .append("circle")
         .attrs({
-            "class" : function(d) { return d.type + " " + d.flow.tags; },
-            "cx": function(d) { return pos_x(lradius, rad(d.index)); },
-            "cy": function(d) { return pos_y(lradius, rad(d.index)); },
-            "r" : cradius
+            "class" : function(d) {
+                return d.data.type + " " + d.data.tags;
+            },
+            "cx" : node_x,
+            "cy" : node_y,
+            "r" : node_size
         })
-        .on("mouseover", function() { edge_mouse_event(this, true); })
-        .on("mouseout", function() { edge_mouse_event(this, false); })
+        .on("mouseover", function() { edge_mouse_event(this, true) })
+        .on("mouseout", function() { edge_mouse_event(this, false) })
         .append("title")
-        .text(descr);
+        .text(function(d) { return d.data.key || d.data.data.rule_str; });
 
     // draw text label
-    // ref: Interactive text rotation with d3.js - bl.ocks.org
-    // http://bl.ocks.org/d3noob/10633421
-
     svg.selectAll("text")
-        .data(all_edges)
+        .data(nodes.descendants())
         .enter()
         .append("text")
         .attrs({
-            "dx": function(d) {
-                var t = rad(d.index);
-                var dx = cradius * 1.2;
-                return (Math.PI/2 < t && t < 3*Math.PI/2) ? -dx : dx;
+            "dx" : function(d) {
+                var angle = node_angle_deg(d.x);
+                var dx = node_size * 1.3;
+                return is_angle_halfside(angle) ? -dx : dx;
             },
-            "dy": cradius/2,
-            "text-anchor": function(d) {
-                var t = rad(d.index);
-                return (Math.PI/2 < t && t < 3*Math.PI/2) ? "end" : "start";
+            "dy" : node_size/2,
+            "text-anchor" : function(d) {
+                var angle = node_angle_deg(d.x);
+                return is_angle_halfside(angle) ? "end" : "start";
             },
             "transform" : function(d) {
-                var t = deg(d.index);
-                var angle = (90 < t && t < 270) ? t + 180 : t;
-                return "translate("
-                    + pos_x(lradius, rad(d.index)) + ","
-                    + pos_y(lradius, rad(d.index)) + ") "
-                    + "rotate(" + angle + ")"; // use degree
+                var angle = node_angle_deg(d.x);
+                angle = is_angle_halfside(angle) ? angle + 180 : angle;
+                return [
+                    "translate(", node_x(d), ",", node_y(d), ")",
+                    "rotate(", angle, ")"
+                ].join("");
             }
         })
-        .text(descr);
+        .text(function(d) {
+            var sp, tp;
+            if(d.data.type === "source") {
+                sp = d.data.source_port;
+                tp = d.data.target_port;
+            } else {
+                tp = d.data.source_port;
+                sp = d.data.target_port;
+            }
+            return d.data.key || [
+                "[", d.data.flow_index, "] ",
+                d.data.switch, " ", sp, " > ", tp
+            ].join("");
+        });
+
+    // mouse event (highlight object)
+    function edge_mouse_event(th, is_mouse_over) {
+        var class_list = th.getAttribute("class").split(" ");
+        var mac_tags = [];
+        var other_tags = [];
+        class_list.forEach(function(tag) {
+            if(tag.match(/mac_/)) {
+                mac_tags.push(tag);
+            } else {
+                [/to_/, /flood/, /rule_/].forEach(function(re) {
+                    if(tag.match(re)) {
+                        other_tags.push(tag);
+                    }
+                });
+            }
+        });
+        var other_tag_str = other_tags.join(".");
+        if(mac_tags.length > 0) {
+            mac_tags.forEach(function(mac_tag) {
+                svg.selectAll(["", mac_tag, other_tag_str].join("."))
+                    .classed("targeted", is_mouse_over);
+            });
+        } else {
+            svg.selectAll(["", other_tag_str].join("."))
+                .classed("targeted", is_mouse_over);
+        }
+    }
 }
 
-function gen_path_index(flow_path) {
-    var src = flow_path.src.index;
-    var dst = flow_path.dst.index;
-    return [src, src, dst, dst];
+function path_angle(x) {
+    return rad(x);
+}
+function rad(deg) {
+    // degree to radian
+    return deg/360 * 2 * Math.PI;
+}
+function is_angle_halfside(x) {
+    // use degree
+    return (90 < x && x < 270);
+}
+function node_angle_deg(x) {
+    return x - 90;
+}
+function node_angle_rad(x) {
+    return rad(node_angle_deg(x));
+}
+function node_x(d) {
+    return d.y * Math.cos(node_angle_rad(d.x));
+}
+function node_y(d) {
+    return d.y * Math.sin(node_angle_rad(d.x));
 }
